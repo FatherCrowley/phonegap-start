386f9222648e9334476a8e0b4e2470b6cfd6fc71
FatherCrowley <teh.legion.27@gmail.com>
2015-04-21 17:46:16 +0100

    a

--- www/js/HeroesOfAldaria.js
+++ www/js/HeroesOfAldaria.js
@@ -1,4 +1,40 @@
-function scrollWin() 
-{
-    window.scrollTo(500, 500);
-}
+var xViewPos;
+var yViewPos;
+var init = false;
+var fps =  60 ;
+var screenWidth;
+var screenHeight;
+var panelWidth;
+var panelHeight;
+var needToScroll;
+var scrollSpeed;
+function Initialize ()
+	{
+		xViewPos = 0;
+		yViewPos = 0;
+		var body = document.body;
+		var html = document.documentElement;
+		screenHeight = Math.max( body.scrollHeight, body.offsetHeight,html.clientHeight, html.scrollHeight, html.offsetHeight );
+		screenWidth  = Math.max( body.scrollWidth, body.offsetWidth,html.clientWidth, html.scrollWidth, html.offsetWidth );
+		panelWidth   = screenWidth / 3;
+		panelHeight  = screenHeight /3;
+		needToScroll = true;
+		scrollSpeed = 10;
+		init = true;
+	}
+function ScrollControl() 
+	{
+		if(needToScroll)
+			window.scrollTo(xViewPos+scrollSpeed, yViewPos+scrollSpeed);
+		xViewPos += scrollSpeed;
+		yViewPos += scrollSpeed;	
+	}
+
+var MainLoop = function() 
+	{	
+		if(!init)
+			Initialize();
+		ScrollControl();
+	};
+	setInterval( MainLoop, 1000 /fps );
+	

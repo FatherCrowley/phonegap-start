21183b7a783c6be03ef0962470d465dfaf2ece5c
FatherCrowley <teh.legion.27@gmail.com>
2015-04-24 16:43:24 +0100

    adadsa

--- www/js/HeroesOfAldaria_Story.js
+++ www/js/HeroesOfAldaria_Story.js
@@ -3,5 +3,33 @@ var charLocation;
 function SetEventsLocation(location)
 {
 	charLocation = location;
+	if(location == "Priceton")
+	{
+		document.getElementById("Priceton").src              = "img/Loc5.png";
+		document.getElementById("Golzbergium").src           = "img/Loc2.png";
+		document.getElementById("Bowersvile").src           = "img/Loc4.png";
+		document.getElementById("TheFieldsOfDevilly").src = "img/Loc1.png";
+	}	
+	if(location == "Golzbergium")
+	{
+		document.getElementById("Priceton").src              = "img/Loc3.png";
+		document.getElementById("Golzbergium").src           = "img/Loc5.png";
+		document.getElementById("Bowersvile").src           = "img/Loc4.png";
+		document.getElementById("TheFieldsOfDevilly").src = "img/Loc1.png";
+	}
+	if(location == "Bowersvile")
+	{
+		document.getElementById("Priceton").src              = "img/Loc3.png";
+		document.getElementById("Golzbergium").src           = "img/Loc2.png";
+		document.getElementById("Bowersvile").src           = "img/Loc5.png";
+		document.getElementById("TheFieldsOfDevilly").src = "img/Loc1.png";
+	}
+	if(location == "The Fields Of Devilly")
+	{
+		document.getElementById("Priceton").src              = "img/Loc3.png";
+		document.getElementById("Golzbergium").src           = "img/Loc2.png";
+		document.getElementById("Bowersvile").src           = "img/Loc4.png";
+		document.getElementById("TheFieldsOfDevilly").src = "img/Loc5.png";
+	}
 	console.log("You went to " + location);
 }

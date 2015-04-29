9ad30eb50a2d67138ca03a35dacdbd4434270b93
FatherCrowley <teh.legion.27@gmail.com>
2015-04-29 10:47:43 +0100

    qqq

--- www/js/HeroesOfAldaria_Story.js
+++ www/js/HeroesOfAldaria_Story.js
@@ -34,6 +34,33 @@ function SetEventsLocation(location)
 	console.log("You went to " + location);
 }
 
+function TextScene(StringArray)
+{
+	this.choinceID = parseInt(StringArray[0],10);
+	this.choiceText = StringArray[1];
+	this.option1  = new TextOption(StringArray[2] ,StringArray[3] ,StringArray[4] ,StringArray[5] ,StringArray[6])
+	this.option2  = new TextOption(StringArray[7] ,StringArray[8] ,StringArray[9] ,StringArray[10],StringArray[11])
+	this.option3  = new TextOption(StringArray[12],StringArray[13],StringArray[14],StringArray[15],StringArray[16])
+	this.option4  = new TextOption(StringArray[17],StringArray[18],StringArray[19],StringArray[20],StringArray[21])	
+}
+TextScene.prototype.Display = function()
+{
+	document.getElementById(HTMLID).innerHTML = this.Text;
+}
+
+
+function TextOption(Text, WinLink, LoseLink, TestStat, TestValue)
+{
+	this.Text = Text;
+	this.WinLink= WinLink;
+	this.LoseLink= LoseLink;
+	this.TestStat= TestStat;
+	this.TestValue= TestValue;
+}
+TextOption.prototype.Display = function(HTMLID)
+{
+	document.getElementById(HTMLID).innerHTML = this.Text;
+}
 function ResetChar()
 {
 	console.log("You reset Everything! you bastard");

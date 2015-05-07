3a2c82c6c70d3997cd22c2d5bd09c031da1f408f
FatherCrowley <teh.legion.27@gmail.com>
2015-05-07 04:21:14 +0100

    hjv

--- www/js/HeroesOfAldaria_FileOperations.js
+++ www/js/HeroesOfAldaria_FileOperations.js
@@ -1,7 +1,7 @@
-document.addEventListener('DOMContentLoaded', ReadStory, false);
-document.addEventListener('DOMContentLoaded', ReadTrophys, false);
-document.addEventListener('DOMContentLoaded', ReadEquipment, false);
-document.addEventListener('DOMContentLoaded', ReadSave, false);
+document.addEventListener('deviceready', ReadStory, false);
+document.addEventListener('deviceready', ReadTrophys, false);
+document.addEventListener('deviceready', ReadEquipment, false);
+document.addEventListener('deviceready', ReadSave, false);
 document.addEventListener("pause",  WriteSave, false);
 var done = 0;
 
